b7a36d52edf2dd3d55c28163d2cb50f40a0ade08
Viviane Araujo <developer.viviane@gmail.com>
2023-06-15 12:44:51 -0300

    Implementado multipla opção de produros

--- chooseProduct.js
+++ chooseProduct.js
@@ -1,5 +1,12 @@
 function chooseProduct(segment) {
-    // Add your solution here!
+    const sabor = "";
+    if(segment === "Beverage"){sabor = "Soda" }
+    if(segment === "Fruit"){sabor = "Apple" }
+    if(segment === "Hygiene"){sabor = "Soap" }
+
+    return sabor
 }
 
 module.exports = chooseProduct;
+
+
